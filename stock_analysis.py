39c04d49f1dda548a928871b2f6fc0445f4cce21
Jeyanth Kallis Sweeton <sweeton.2001@outlook.com>
2023-10-11 21:09:13 +0530

    Scikit learn prediction

--- stock_analysis.py
+++ stock_analysis.py
@@ -2,10 +2,12 @@ import matplotlib
 matplotlib.use('Agg')
 
 
-from flask import Flask, render_template, request, send_file  # Import send_file
+from flask import Flask, render_template, request, send_file
 from alpha_vantage.timeseries import TimeSeries
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 from io import BytesIO
+import pandas as pd
+import matplotlib.pyplot as plt
 import base64
 
 app = Flask(__name__)
@@ -22,6 +24,31 @@ def index():
 
         # Fetch stock data from Alpha Vantage
         stock_data, meta_data = ts.get_daily(symbol=selected_stock, outputsize="compact")
+        if request.form.get("predict_price"):
+            # Fetch historical stock data
+            stock_data, _ = ts.get_daily(symbol=selected_stock, outputsize="full")
+
+            # Extract closing price and date
+            stock_data = stock_data[['4. close']]
+            stock_data.reset_index(inplace=True)
+
+            # Convert the date to a numerical format (e.g., Unix timestamp)
+            stock_data['date'] = stock_data['date'].astype('datetime64[ns]').view('int64') // 10**9
+
+            # Train a linear regression model
+            X = stock_data[['date']]
+            y = stock_data['4. close']
+            model = LinearRegression()
+            model.fit(X, y)
+
+            # Convert the selected date to the same format
+            selected_date = pd.Timestamp(request.form["selected_date"]).value // 10**9
+
+            # Predict the stock price for the selected date
+            predicted_price = model.predict([[selected_date]])[0]
+
+            # Render the prediction result
+            return render_template("index.html", predicted_price=predicted_price)
 
         if request.form.get("plot_graph"):  # Check if the plot_graph button was clicked
             # Create a Matplotlib graph with the closing price
